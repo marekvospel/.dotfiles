9cc0bcb642ebdf2ff7b49aa5495b39ddd021d31f
Marek Vospěl <marek@vospel.cz>
2024-09-08 23:16:26 +0200

    feat: add tmux config, nvim theme

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -5,6 +5,7 @@ return {
     opts = {
       ensure_installed = {
         "rust-analyzer",
+	"clangd",
       }
     }
   },
@@ -14,14 +15,32 @@ return {
       require "plugins.configs.lspconfig"
     end,
   },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme catppuccin]])
+    end
+  },
   -- Syntax highlight
   "nvim-treesitter/nvim-treesitter",
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    keys = {
+        ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+        ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+        ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+        ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+    }
+  },
   -- Tree
   {
     "nvim-tree/nvim-tree.lua",
     keys = {
       { "<C-n>", "<cmd>NvimTreeToggle<cr>", desc = "Toggle nvim tree" },
-
     },
     config = function()
       require("nvim-tree").setup {}
