dd5cf4dafd5218a3523025bc0414361f1fe76bf6
Marek Vospěl <marek@vospel.cz>
2025-01-24 14:10:43 +0100

    feat: nvim comment, improve keybinds, improve docs

--- .config/nvim/lua/config/debug/init.lua
+++ .config/nvim/lua/config/debug/init.lua
@@ -10,8 +10,20 @@ M.setup = function()
   vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, {})
   vim.keymap.set('n', '<leader>dc', dap.continue, {})
   vim.keymap.set('n', '<leader>dt', dap.terminate, {})
+  vim.keymap.set('n', '<leader>ds', dap.session, {})
+  vim.keymap.set('n', '<leader>dC', dap.run_to_cursor, {})
+  vim.keymap.set('n', '<leader>di', dap.step_into, {})
+  vim.keymap.set('n', '<leader>do', dap.step_out, {})
+  vim.keymap.set('n', '<leader>dO', dap.step_over, {})
+
+  vim.keymap.set('n', '<leader>dj', dap.down, {})
+  vim.keymap.set('n', '<leader>dk', dap.up, {})
 
   vim.keymap.set('n', '<leader>dr', dap.repl.toggle, {})
+  vim.keymap.set('n', '<leader>dw', function()
+    require('dap.ui.widgets').hover()
+  end, {})
+  -- Maybe dg (goto), dP (pause), dl (run last)
 
   -- Adapters, configs
   require('config.debug.c').setup()
