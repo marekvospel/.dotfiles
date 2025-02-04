384f6d98738d69d289d7972382b2adaa8fc59ff3
Marek Vospěl <marek@vospel.cz>
2025-02-04 03:33:06 +0100

    feat(nvim): lsp completions, two spaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,12 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+
+-- Indent
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.tabstop=2
+vim.opt.softtabstop = 2
+
 require("config.lazy")
