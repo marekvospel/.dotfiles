6739f23ab8089de444b9d0113fb660d2d2ada19c
Marek Vospěl <marek@vospel.cz>
2025-02-08 22:20:19 +0100

    chore(nvim): firefox launch adpter

--- .config/nvim/lua/config/debug/node.lua
+++ .config/nvim/lua/config/debug/node.lua
@@ -3,6 +3,8 @@ local M = {}
 local dap = require('dap')
 local mason_registry = require('mason-registry')
 local js_dap_path = mason_registry.get_package('js-debug-adapter'):get_install_path() .. '/js-debug-adapter'
+local firefox_dap_path = mason_registry.get_package('firefox-debug-adapter'):get_install_path()
+  .. '/firefox-debug-adapter'
 
 M.setup = function()
   dap.adapters['pwa-node'] = {
@@ -14,7 +16,10 @@ M.setup = function()
       args = { '${port}' },
     },
   }
-  -- TODO: Firefox / chrome launch adapter
+  dap.adapters.firefox = {
+    type = 'executable',
+    command = firefox_dap_path,
+  }
 end
 
 return M
