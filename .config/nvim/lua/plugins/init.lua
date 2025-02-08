a482dbc7e419b0923927f5309d5da5862bce5d99
Marek Vospěl <marek@vospel.cz>
2025-02-08 22:05:23 +0100

    fix(nvim): improve completions, cargo root pattern

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -33,14 +33,34 @@ return {
         'hrsh7th/cmp-path',
       },
     },
-    opts = {
-      sources = {
-        { name = 'nvim_lsp' },
-        { name = 'buffer' },
-        { name = 'nvim_lua' },
-        { name = 'path' },
-      },
-    },
+    opts = function()
+      local cmp = require('cmp')
+
+      local options = {
+        completion = { completeopt = "menu,menuone" },
+        mapping = {
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Enter>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+
+          ["<CR>"] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+          },
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'buffer' },
+          { name = 'nvim_lua' },
+          { name = 'path' },
+        },
+      }
+
+      return options
+    end,
   },
   {
     'stevearc/conform.nvim',
